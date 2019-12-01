a4ebb51a1aa464f8383f238530bfe72e68c73cfc
pchataignier <pedroxubackc@gmail.com>
2019-11-30 22:05:31 -0300

    TF Records generation

--- TFObjectDetectionAPI/generate_tfrecords.py
+++ TFObjectDetectionAPI/generate_tfrecords.py
@@ -1,13 +1,11 @@
 import os
+import argparse
 import pandas as pd
 import tensorflow as tf
+import contextlib2
+from object_detection.dataset_tools import tf_record_creation_util
 from object_detection.utils import dataset_util, label_map_util
 
-
-flags = tf.app.flags
-flags.DEFINE_string('output_dir', '', 'Path to output directory')
-flags.DEFINE_string('input_dir', '', 'Path to data directory')
-FLAGS = flags.FLAGS
 
 def create_label_map(label_map_path, labels):
     with open(label_map_path, 'w+') as f:
@@ -18,26 +16,27 @@ def create_label_map(label_map_path, labels):
                             f'display_name: "{row.ClassName}"}}\n'
             f.write(line)
 
-def create_tf_example(example):
-    # TODO(user): Populate the following variables from your example.
-    height = None # Image height
-    width = None # Image width
-    filename = None # Filename of the image. Empty if image is not from file
-    encoded_image_data = None # Encoded image bytes
-    image_format = None # b'jpeg' or b'png'
 
-    xmins = [] # List of normalized left x coordinates in bounding box (1 per box)
-    xmaxs = [] # List of normalized right x coordinates in bounding box
-             # (1 per box)
-    ymins = [] # List of normalized top y coordinates in bounding box (1 per box)
-    ymaxs = [] # List of normalized bottom y coordinates in bounding box
-             # (1 per box)
-    classes_text = [] # List of string class name of bounding box (1 per box)
-    classes = [] # List of integer class id of bounding box (1 per box)
+def create_tf_example(example, split):
+    height = example.Height # Image height
+    width = example.Width # Image width
+    filename = bytes(os.path.join(split, os.path.basename(example.FilePath)), 'utf-8') # Filename of the image. Empty if image is not from file
+
+    with tf.io.gfile.GFile(example.FilePath, 'rb') as fid:
+        encoded_image_data = bytes(fid.read()) # Encoded image bytes
+    image_format = b'jpeg' # b'jpeg' or b'png'
+
+    xmins = example.XMin # List of normalized left x coordinates in bounding box (1 per box)
+    xmaxs = example.XMax # List of normalized right x coordinates in bounding box (1 per box)
+    ymins = example.YMin # List of normalized top y coordinates in bounding box (1 per box)
+    ymaxs = example.YMax # List of normalized bottom y coordinates in bounding box (1 per box)
+
+    classes_text = [x.encode() for x in example.LabelName] # List of string class name of bounding box (1 per box)
+    classes = example.LabelID # List of integer class id of bounding box (1 per box)
 
     tf_example = tf.train.Example(features=tf.train.Features(feature={
-        'image/height': dataset_util.int64_feature(height),
-        'image/width': dataset_util.int64_feature(width),
+        'image/height': dataset_util.int64_feature(int(height)),
+        'image/width': dataset_util.int64_feature(int(width)),
         'image/filename': dataset_util.bytes_feature(filename),
         'image/source_id': dataset_util.bytes_feature(filename),
         'image/encoded': dataset_util.bytes_feature(encoded_image_data),
@@ -52,33 +51,59 @@ def create_tf_example(example):
     return tf_example
 
 
-def main(_):
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description="Generates Tensorflow '.record' files from downloaded images")
+    parser.add_argument('-i', '--input_dir', required=False, default="",
+                        help="Path to input directory. Default is current directory")
+    parser.add_argument('-o', '--output_dir', required=False, default="",
+                        help="Path to desired output directory. "
+                             "If specified, saves files in <outdir>/[train, test, validation], "
+                             "otherwise saves them to <working directory>/[train, test, validation]")
+    parser.add_argument('-s', '--shards', required=False, type=int)
+    args = parser.parse_args()
+
+    pd.set_option("display.max_colwidth", 10000)
+
+    #TODO: Loop over splits
     split = "validation"
 
-    record_path = os.path.join(FLAGS.output_dir, split, f"{split}.record")
-    writer = tf.python_io.TFRecordWriter(record_path)
-
-    class_label_file = os.path.join(FLAGS.input_dir, "filteredLabels.csv")
-    bbox_file = os.path.join(FLAGS.input_dir, split, f"{split}-filtered-bbox.csv")
-    img_ids_file = os.path.join(FLAGS.input_dir, split, f"{split}-filtered-image-ids.csv")
-    download_summary_file = os.path.join(FLAGS.input_dir, split, f"{split}_download_summary.csv")
+    class_label_file = os.path.join(args.input_dir, "filteredLabels.csv")
+    bbox_file = os.path.join(args.input_dir, split, f"{split}-filtered-bbox.csv")
+    download_summary_file = os.path.join(args.input_dir, split, f"{split}-download-summary.csv")
 
     labels = pd.read_csv(class_label_file)
     bboxes = pd.read_csv(bbox_file, escapechar='"')
-    img_ids = pd.read_csv(img_ids_file)
     download_summary = pd.read_csv(download_summary_file)
 
-    label_map_path = os.path.join(FLAGS.output_dir, "labelMap.pbtxt")
+    label_map_path = os.path.join(args.output_dir, "labelMap.pbtxt")
     create_label_map(label_map_path, labels)
     label_dict = label_map_util.get_label_map_dict(label_map_util.load_labelmap(label_map_path))
-    # TODO(user): Write code to read in your dataset to examples variable
 
-    for example in examples:
-        tf_example = create_tf_example(example)
-        writer.write(tf_example.SerializeToString())
+    bboxes = bboxes[bboxes.ImageID.isin(download_summary[~download_summary.FilePath.isnull()].ImageID)]
+    bboxes["LabelID"] = bboxes.LabelName.apply(lambda x: label_dict[x])
+    examples = bboxes.groupby("ImageID").agg({"LabelName": list, "LabelID":list,
+                                              "XMin": list, "XMax": list,
+                                              "YMin": list, "YMax": list}).reset_index()
+    examples = pd.merge(examples, download_summary, on="ImageID")
 
-    writer.close()
+    record_path = os.path.join(args.output_dir, split, f"{split}.record")
 
+    if not args.shards:
+        writer = tf.io.TFRecordWriter(record_path)
+        for example in examples.itertuples(index=False):
+            tf_example = create_tf_example(example, split)
+            writer.write(tf_example.SerializeToString())
 
-if __name__ == '__main__':
-    tf.app.run()
+        writer.close()
+
+    else: #Sharded records
+        num_shards=args.shards
+
+        with contextlib2.ExitStack() as tf_record_close_stack:
+            output_tfrecords = tf_record_creation_util.open_sharded_output_tfrecords(tf_record_close_stack, record_path, num_shards)
+
+            for example in examples.itertuples():
+                tf_example = create_tf_example(example)
+                output_shard_index = example.Index % num_shards
+                output_tfrecords[output_shard_index].write(tf_example.SerializeToString())
